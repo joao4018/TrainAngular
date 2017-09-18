5e4335ce7e6662e16ef418ba9f9d87f9fea40d01
joao4018 <joao4018@gmail.com>
2017-09-18 00:54:15 -0300

    Treino-Basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
-}
+  students: Student[] = [
+    {name: 'luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'cola', isJedi: false, temple: 'Coruscant'},
+    {name: 'leia', isJedi: true, temple: ''}
+  ]
+    }
